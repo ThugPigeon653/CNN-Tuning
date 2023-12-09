bfcdcb4ba97768bd53bdcab11a6d21c1a3416027
ThugPigeon653 <89567974+ThugPigeon653@users.noreply.github.com>
2023-12-09 14:40:26 +1000

    altered caching for faster build

--- config/app.py
+++ config/app.py
@@ -1,21 +1,21 @@
 from flask import Flask, Response
-import yaml
+import json
 
 app = Flask(__name__)
 
 @app.route('/', methods=["GET"])
-def get_yaml():
-    yaml_file_path = "config.yaml"
+def get_json():
+    json_file_path = "config.json"
 
     try:
-        with open(yaml_file_path, 'r') as file:
-            yaml_content = file.read()
-        response = Response(yaml_content, content_type="text/yaml")
+        with open(json_file_path, 'r') as file:
+            json_content = file.read()
+        response = Response(json_content, content_type="application/json")
         return response
     except FileNotFoundError:
         return "Config file not found", 404
     except Exception as e:
         return str(e), 500
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
--- training-data/app.py
+++ training-data/app.py
@@ -1,8 +1,19 @@
-from flask import Flask
+from flask import Flask, jsonify
 import tensorflow as tf
 
-app=Flask(__name__)
+app = Flask(__name__)
 
 @app.route('/', methods=["GET"])
 def get_training_data():
-    return tf.keras.datasets.mnist
+    try:
+        (x_train, y_train), _ = tf.keras.datasets.mnist.load_data()
+
+        mnist_data = {'x_train': x_train.tolist(), 'y_train': y_train.tolist()}
+        print("DATASET LOADED")
+        return jsonify(mnist_data)
+    except Exception as e:
+        print(f"Error loading training data: {e}")
+        return jsonify({'error': 'Failed to load training data'}), 500
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
